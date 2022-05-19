38845b05ca48ba5c7313a0f458951ba24be91e23
ThatcherK <kaliithatmau@gmail.com>
2022-05-19 10:15:59 +0300

    Add bulbs

--- src/components/AddBulb/AddBulb.tsx
+++ src/components/AddBulb/AddBulb.tsx
@@ -1,12 +1,34 @@
 import { useState } from 'react';
+import { v4 as uuidv4 } from 'uuid';
 import { Form, Modal } from 'react-bootstrap';
 import Button from 'react-bootstrap/Button';
 
+interface Bulbs{
+    id: string;
+    colour: string;
+    status: string;
+}
+
 function AddBulb () {
     const [show, setShow] = useState(false);
+    const [colour, setColour] = useState('');
+    const [bulbs, setBulbs] = useState<Bulbs[]>([]);
   
     const handleClose = () => setShow(false);
     const handleShow = () => setShow(true);
+    const handleSelectedColor = (event:any) => {
+        setColour(event.target.value)
+    }
+
+    const handleAddBulb = () => {
+        const bulbDetails = {
+            id: uuidv4(),
+            colour: colour,
+            status: 'disabled'
+        }
+        setBulbs(bulbs.concat(bulbDetails))
+        setShow(false)
+    }
 
     return (
     <>
@@ -16,19 +38,19 @@ function AddBulb () {
             <Modal.Title>Add a bulb</Modal.Title>
         </Modal.Header>
         <Modal.Body>
-            <Form.Select aria-label="Default select example">
+            <Form.Select aria-label="Default select example" onChange={(e) => handleSelectedColor(e)}>
                 <option>Select bulb colour</option>
-                <option value="1">Red</option>
-                <option value="2">Yellow</option>
-                <option value="3">Blue</option>
-                <option value="4">Green</option>
+                <option value="red">Red</option>
+                <option value="yellow">Yellow</option>
+                <option value="blue">Blue</option>
+                <option value="green">Green</option>
             </Form.Select>
         </Modal.Body>
         <Modal.Footer>
             <Button variant="secondary" onClick={handleClose}>
                 Close
             </Button>
-            <Button variant="primary" onClick={handleClose}>
+            <Button variant="primary" onClick={handleAddBulb}>
                 Add
             </Button>
         </Modal.Footer>
